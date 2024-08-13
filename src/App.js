6af8844f2caf79e92c744423104375156f717156
Dhruva Kumar <dhr7va@gmail.com>
2024-08-13 15:00:20 +0530

    seperated the date

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import ExpenseItems from "./components/ExpenseItems";
 function App() {
+  const expenses = [
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100, location: "Bangalore" },
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10, location: "Delhi" },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1, location: "Hyderabad" },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200, location: "Mumbai" },
+  ];
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItems
-        date={new Date(2023, 7, 15)}
-        title="Insurance"
-        price="50"
-        location="Bangalore"
-      />
-      <ExpenseItems
-        date={new Date(2023, 3, 25)}
-        title="Book"
-        price="20"
-        location="Delhi"
-      />
-      <ExpenseItems
-        date={new Date(2023, 10, 11)}
-        title="Pen"
-        price="5"
-        location="Hyderabad"
-      />
-      <ExpenseItems
-        date={new Date(2023, 1, 14)}
-        title="Laptop"
-        price="200"
-        location="Mumbai"
-      />
+      {
+        expenses.map((expense, index) => {
+          return (
+            <ExpenseItems
+              key={expense.id}
+              date={expense.date}
+              title={expense.title}
+              price={expense.price}
+              location={expense.location}
+            />
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,15 +1,34 @@
-
 import "./ExpenseItem.css";
 
-export default function ExpenseItem(prop) {
+export default function ExpenseItems(props) {
+    const months = ['January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+    const month = months[props.date.getMonth()];
+    const year = props.date.getFullYear();
+    const date = props.date.getDate();
     return (
         <div className="expense-item">
-            <div>{prop.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{prop.title}</h2>
-                <div className="expense-item__price">${prop.title}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{date}</div>
             </div>
-            <div className="expense-item__location">{prop.location}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.price}</div>
+            </div>
+            <div className="expense-item__location">{props.location}</div>
         </div>
     );
 }
