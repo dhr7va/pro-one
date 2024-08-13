541dfdcd2dca9a723b45dc9b32e2c974b4318b7a
Dhruva Kumar <dhr7va@gmail.com>
2024-08-13 12:46:19 +0530

    added css

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,12 +1,17 @@
-export default function ExpenseItem() {
+
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
     return (
-        <div>
+        <div className="expense-item">
             <div>15 August 2023</div>
-            <div>
+            <div className="expense-item__description">
                 <h2>Book</h2>
-                <p> $10</p>
+                <div className="expense-item__price">$10</div>
             </div>
-            <div> Delhi</div>
+            <div className="expense-item__location">Delhi</div>
         </div>
-    )
+    );
 }
+
+export default ExpenseItem;
