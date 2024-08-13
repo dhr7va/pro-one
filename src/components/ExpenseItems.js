9dab797095b218909b862fc851017933959580a5
Dhruva Kumar <dhr7va@gmail.com>
2024-08-13 12:38:54 +0530

    expense app

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,9 @@
+export default function ExpenseItems() {
+    return (
+        <ul>
+            <li>Food - $5</li>
+            <li>Petrol - $20</li>
+            <li>Movie - $ 10</li>
+        </ul>
+    )
+}
