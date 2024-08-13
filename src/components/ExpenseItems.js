bc5e087233180f933cdd17aa0825529e4b4c0dff
Dhruva Kumar <dhr7va@gmail.com>
2024-08-13 12:40:08 +0530

    Added details to the expense items

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,9 +1,12 @@
-export default function ExpenseItems() {
+export default function ExpenseItem() {
     return (
-        <ul>
-            <li>Food - $5</li>
-            <li>Petrol - $20</li>
-            <li>Movie - $ 10</li>
-        </ul>
+        <div>
+            <div>15 August 2023</div>
+            <div>
+                <h2>Book</h2>
+                <p> $10</p>
+            </div>
+            <div> Delhi</div>
+        </div>
     )
 }
