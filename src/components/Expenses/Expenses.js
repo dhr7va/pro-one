0d65fcb145eb923d3895cd48e14684b086d16f32
Dhruva Kumar <dhr7va@gmail.com>
2024-08-15 22:23:44 +0530

    added the elements directly form the form

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,25 +1,23 @@
-import "./Expenses.css";
+import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
 
-export default function Expenses() {
-    const expenses = [
-        { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100, location: "Bangalore" },
-        { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10, location: "Delhi" },
-        { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1, location: "Hyderabad" },
-        { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200, location: "Mumbai" },
-    ];
-
+const Expenses = (props) => {
     return (
-        <div className="expenses">
-            {expenses.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    date={expense.date}
-                    title={expense.title}
-                    price={expense.price}
-                    location={expense.location}
-                />
-            ))}
-        </div>
+        <Card className="expenses">
+            {props.expenses.map((expense) => {
+                return (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        date={expense.date}
+                        price={expense.price}
+                    />
+                );
+            })}
+        </Card>
     );
-}
+};
+
+export default Expenses;
